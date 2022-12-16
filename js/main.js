96c0ae26ef61dba53c5964f69014adc05bf940ae
descentss <engspesee@gmail.com>
2022-12-16 18:24:28 +0900

    babel integration

--- js/main.js
+++ js/main.js
@@ -1,52 +1,3 @@
-if (!("classList" in document.documentElement) && Object.defineProperty && typeof HTMLElement !== 'undefined') {
-  Object.defineProperty(HTMLElement.prototype, 'classList', {
-      get: function() {
-          var self = this;
-          function update(fn) {
-              return function(value) {
-                  var classes = self.className.split(/\s+/),
-                      index = classes.indexOf(value);
-
-                  fn(classes, index, value);
-                  self.className = classes.join(" ");
-              }
-          }
-
-          var ret = {                    
-              add: update(function(classes, index, value) {
-                  ~index || classes.push(value);
-              }),
-
-              remove: update(function(classes, index) {
-                  ~index && classes.splice(index, 1);
-              }),
-
-              toggle: update(function(classes, index, value) {
-                  ~index ? classes.splice(index, 1) : classes.push(value);
-              }),
-
-              contains: function(value) {
-                  return !!~self.className.split(/\s+/).indexOf(value);
-              },
-
-              item: function(i) {
-                  return self.className.split(/\s+/)[i] || null;
-              }
-          };
-          
-          Object.defineProperty(ret, 'length', {
-              get: function() {
-                  return self.className.split(/\s+/).length;
-              }
-          });
-
-          return ret;
-      }
-  });
-}
-
-
-//
 //    Toggle Mobile Navigation
 //
 const navbarMenu = document.querySelector("#navigation #navbar-menu");
@@ -64,18 +15,17 @@ hamburgerMenu.addEventListener('click', function () {
 
 document.addEventListener('scroll', (e) => {
   const scroll = document.documentElement.scrollTop;
-  if (scroll >= 100) {
-      document.querySelector('body').classList.add('scroll')
-  } 
-  else {
-      document.querySelector('body').classList.remove('scroll')
+    if (scroll >= 100) {
+      document.querySelector('body').classList.add('scroll');
+  } else {
+      document.querySelector('body').classList.remove('scroll');
   }
 });
+
 
 //
 //    Toggle Image Animation on scroll
 //
-
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
@@ -88,9 +38,9 @@ const observer = new IntersectionObserver((entries) => {
   })
 })
 
+
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((e1) => observer.observe(e1));
-
 
 
 
