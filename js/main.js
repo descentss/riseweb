574d3e3c41be02d01f362c9dcd33fc1776d316db
descentss <engspesee@gmail.com>
2022-12-16 15:47:36 +0900

    added polyfills

--- js/main.js
+++ js/main.js
@@ -1,48 +1,3 @@
-//
-//    Toggle Mobile Navigation
-//
-const navbarMenu = document.querySelector("#navigation #navbar-menu");
-const hamburgerMenu = document.querySelector("#navigation .hamburger-menu");
-
-hamburgerMenu.addEventListener('click', function () {
-  navbarMenu.classList.toggle("open");
-  hamburgerMenu.classList.toggle("clicked");
-});
-
-
-//
-//    Toggle Navigation Light Mode
-//
-
-document.addEventListener('scroll', (e) => {
-  const scroll = document.documentElement.scrollTop;
-  if (scroll >= 100) {
-      document.querySelector('body').classList.add('scroll')
-  } 
-  else {
-      document.querySelector('body').className.remove('scroll')
-  }
-});
-
-//
-//    Toggle Image Animation on scroll
-//
-
-
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-          entry.target.classList.add("show");
-      } 
-      else {
-          entry.target.classList.remove("show");
-      }
-  })
-})
-
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((e1) => observer.observe(e1));
-
 if (!("classList" in document.documentElement) && Object.defineProperty && typeof HTMLElement !== 'undefined') {
   Object.defineProperty(HTMLElement.prototype, 'classList', {
       get: function() {
@@ -89,6 +44,54 @@ if (!("classList" in document.documentElement) && Object.defineProperty && typeo
       }
   });
 }
+
+
+//
+//    Toggle Mobile Navigation
+//
+const navbarMenu = document.querySelector("#navigation #navbar-menu");
+const hamburgerMenu = document.querySelector("#navigation .hamburger-menu");
+
+hamburgerMenu.addEventListener('click', function () {
+  navbarMenu.classList.toggle("open");
+  hamburgerMenu.classList.toggle("clicked");
+});
+
+
+//
+//    Toggle Navigation Light Mode
+//
+
+document.addEventListener('scroll', (e) => {
+  const scroll = document.documentElement.scrollTop;
+  if (scroll >= 100) {
+      document.querySelector('body').classList.add('scroll')
+  } 
+  else {
+      document.querySelector('body').classList.remove('scroll')
+  }
+});
+
+//
+//    Toggle Image Animation on scroll
+//
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+      } 
+      else {
+          entry.target.classList.remove("show");
+      }
+  })
+})
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((e1) => observer.observe(e1));
+
+
 
 
 
